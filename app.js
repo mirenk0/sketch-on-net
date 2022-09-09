676a2f3e3f359fb9a6654541bd937badf1d59271
mirenk0 <mirenkodaci21@gmail.com>
2022-09-09 14:19:22 +0200

    Add paint functionality

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 let canvas = document.querySelector(".canvas");
 
-let numberOfPixels = 16;
+let numberOfPixels = 64;
 
 let resolution = Math.pow(numberOfPixels, 2);
 
+let color = "red";
+
 
 //Create a grid
+//change color
+
 
 function createPixels(number) {
     canvas.style.setProperty('--grid-rows', numberOfPixels);
     canvas.style.setProperty('--grid-cols', numberOfPixels);
     for(let i = 0; i < number; i++){
-        let pixel = document.createElement("div");
+        var pixel = document.createElement("div");
         canvas.appendChild(pixel).className = "pixel-grid";
-    }
+    } 
+    pixel.style.setProperty("--background-color", color); 
 }
 
 
